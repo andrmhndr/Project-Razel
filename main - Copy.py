00f612ae01f45b3949b117e4444c3a72f1dc7806
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2023-05-08 19:00:58 +0700

    update

---
+++ main - Copy.py
@@ -0,0 +1,24 @@
+import customtkinter as gui
+from datetime import datetime
+import json
+
+from repository.data_repository import DataRepository
+from view.main_view import *
+from model.dialog_model import *
+from controller.main_controller import *
+from controller.thread_controller import *
+
+def main():
+    # initialize
+    today = datetime.now().strftime("%Y-%m-%d")
+    record_data = json.load(open('data/record.json'))
+    api_data = json.load(open('data/data.json'))
+    
+    repository = DataRepository(today, api_data, record_data)
+    view = MainView(root, "Project-Razel")
+    controller = MainController(view, repository)
+    
+    root.mainloop()
+    
+if __name__ == '__main__':
+    main()
